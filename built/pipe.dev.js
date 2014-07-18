95d7b71e71d9fcb755f0abc33c4504d18bb0acc9
simonfan <simonxd@gmail.com>
2014-07-18 17:43:40 -0300

    cache bug

--- built/pipe.dev.js
+++ built/pipe.dev.js
@@ -65,12 +65,14 @@ define('__pipe/drain',['require','exports','module','lodash'],function (require,
 		// [3]
 		_.each(properties, function (destProps, srcProp) {
 
+			// [0] get the dest prop
+			var destProp = destProps[0];
 
 			// [1] GET value from the first DESTINATION (dests[0])
-			var value = this._destGet(this.dest, destProps[0]);
+			var value = this._destGet(this.dest, destProp);
 
 			// [2] check cache
-			if (!this.isCached(srcProp, value) || force) {
+			if (!this.isCached(destProp, value) || force) {
 				// [2.1] SET value onto SOURCE
 				this._srcSet(this.src, srcProp, value);
 			}
--- src/__pipe/drain.js
+++ src/__pipe/drain.js
@@ -21,12 +21,14 @@ define(function (require, exports, module) {
 		// [3]
 		_.each(properties, function (destProps, srcProp) {
 
+			// [0] get the dest prop
+			var destProp = destProps[0];
 
 			// [1] GET value from the first DESTINATION (dests[0])
-			var value = this._destGet(this.dest, destProps[0]);
+			var value = this._destGet(this.dest, destProp);
 
 			// [2] check cache
-			if (!this.isCached(srcProp, value) || force) {
+			if (!this.isCached(destProp, value) || force) {
 				// [2.1] SET value onto SOURCE
 				this._srcSet(this.src, srcProp, value);
 			}
